9d222fb96f0ca49dd2a8fe4b2100be13736eaf97
cabralwd <cabralwd@icloud.com>
2021-09-19 03:41:44 -0300

    components header and exit

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="container">
+    <h1>Fire Dragons</h1>
+    <router-link to="/login">deslogar</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 30px;
+  padding-bottom: 30px;
+
+  h1 {
+    font-family: "DragonForcE", Impact, Haettenschweiler, "Arial Narrow Bold",
+      sans-serif;
+    font-weight: normal;
+    font-style: normal;
+    font-size: 1.875rem;
+    text-align: center;
+    color: $three;
+  }
+}
+</style>
